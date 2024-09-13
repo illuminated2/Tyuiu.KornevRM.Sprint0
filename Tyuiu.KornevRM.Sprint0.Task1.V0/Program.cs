858e28492f34e1d5921473f4912cf31db5872afb
illuminated2 <illuminated_89@mail.ru>
2024-09-13 20:31:35 +0500

    Task1

---
+++ Tyuiu.KornevRM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KornevRM.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Руслан";
+            int age = 17;
+            Console.ReadKey();
+            Console.WriteLine("Здравствуйте, меня зовут " + name + "Мне" + age + "лет!");
+        }
+    }
+}
